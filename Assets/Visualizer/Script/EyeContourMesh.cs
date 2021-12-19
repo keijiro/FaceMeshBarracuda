f5d4b72655b9517d5422fb2c8c11490dca6c4bf8
Ishikawa Tatsuya <ishikawa@imgsrc.co.jp>
2021-12-19 20:24:45 +0900

    Add MeshVisualizer Scene

---
+++ Assets/Visualizer/Script/EyeContourMesh.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MediaPipe.FaceMesh
+{
+    public class EyeContourMesh : MonoBehaviour
+    {
+        Mesh _mesh;
+        Material _material;
+
+        class MeshParams
+        {
+            public List<Vector3> vertices;
+
+            public List<Vector2> uv;
+
+            public List<int> triangles;
+
+            public MeshParams()
+            {
+                vertices = new();
+                uv = new();
+                triangles = new();
+            }
+
+        }
+     
+        void Start()
+        {
+
+            Shader shader = Shader.Find("Hidden/MediaPipe/FaceMesh/EyeContourMesh");
+
+            _material = new Material(shader);
+
+            _mesh = new Mesh();
+
+
+            //Setup mesh for eye contour
+
+            MeshParams meshParams = new();
+
+            //Setup vertex, UV
+            for (int i = 0; i < 17; i++)
+            {
+                meshParams.vertices.Add(new Vector3(0, 0, 0));
+
+                meshParams.vertices.Add(new Vector2(0, 0));
+            }
+
+            //Setup Triangles
+            for (int i = 0; i < 7; i++)
+            {
+
+                int[] triangle =
+                    {
+                        i, i+9, i+1,
+                        i+1, i+9, i+10
+                        };
+
+                meshParams.triangles.AddRange(triangle);
+            }
+
+            _mesh.SetVertices(meshParams.vertices);
+            _mesh.SetUVs(0, meshParams.uv);
+            _mesh.SetTriangles(meshParams.triangles, 0);
+
+        }
+
+      
+        public void UpdateMesh(ComputeBuffer vertexBuffer)
+        {
+            _material.SetBuffer("_Vertices", vertexBuffer);
+        }
+
+        public void Draw(Texture texture)
+        {
+            _material.SetTexture("_MainTex", texture);
+            
+            Graphics.DrawMesh(_mesh, transform.position, transform.rotation, _material, 0);
+        }
+    }
+}
---
+++ Assets/Visualizer/Script/FaceMesh.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MediaPipe.FaceMesh
+{
+    public class FaceMesh : MonoBehaviour
+    {
+        [SerializeField] ResourceSet _resource;
+
+        Mesh _mesh;
+        Material _material;
+
+        void Start()
+        {
+
+            Shader shader = Shader.Find("Hidden/MediaPipe/FaceMesh/FaceMesh");
+
+            _material = new Material(shader);
+
+            _mesh = _resource.faceMeshTemplate;
+
+        }
+
+
+        public void UpdateMesh(ComputeBuffer vertexBuffer)
+        {
+            _material.SetBuffer("_Vertices", vertexBuffer);
+        }
+
+        public void Draw(Texture texture)
+        {
+            _material.SetTexture("_MainTex", texture);
+
+            Graphics.DrawMesh(_mesh, transform.position, transform.rotation, _material, 0);
+        }
+    }
+}
