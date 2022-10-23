d4e23a8d39533eb468a2b22cdf5e5937fa66e050
Ishikawa Tatsuya <cpksj887@gmail.com>
2022-10-23 14:00:20 +0900

    FullscreenEditorでgameVIewからフォーカスが外れる処理が含まれていたのでコメントアウト

--- Assets/Fullscreen/Editor/FixGameViewMouseInput.cs
+++ Assets/Fullscreen/Editor/FixGameViewMouseInput.cs
@@ -10,16 +10,16 @@ namespace FullscreenEditor {
         }
 
         public static void UpdateGameViewArea(FullscreenContainer fs) {
-            After.Frames(50, () => {
-                var window = fs.ActualViewPyramid.Window;
+          After.Frames(50, () => {
+                   var window = fs.ActualViewPyramid.Window;
                 if (window && window.IsOfType(Types.PlayModeView)) {
                     Logger.Debug("Fixing game view area");
-                    FullscreenUtility.FocusView(FullscreenUtility.GetMainView());
+                    //FullscreenUtility.FocusView(FullscreenUtility.GetMainView());//gameViewからフォーカスが外れてしまう
 
                     // Issue #95, fix Input.mouseScrollDelta
                     window.Focus();
                 }
-            });
+          });
         }
 
     }
